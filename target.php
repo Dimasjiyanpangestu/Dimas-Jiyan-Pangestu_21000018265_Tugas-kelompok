e6165e0b0d514f7ba0f53e03e76a181e7487d7fc
DIMAS JIYAN PANGESTU <101696911+Dimasjiyanpangestu@users.noreply.github.com>
2022-04-23 23:14:43 +0700

    Add files via upload

---
+++ target.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="contoh4.css">
+	<title></title>
+</head>
+<body>
+	<h1 style="text-align: justify-all;">SELAMAT DATANG DITOKO KAMI</h1>
+	<?php
+	echo "Nama: ".$_POST['dname']."<br/>";
+	echo "Alamat: ".$_POST['alamat']."<br/>";
+	echo "Tanggal: ".$_POST['tanggal']."<br/>";
+	echo "jenis_kelamin: ".$_POST['jeniskelamin']."<br/><br><br>";
+
+?>
+	<table border="1" width="100%">
+		<thead>
+				<table border="0"width="100%">
+					<tr>
+						<td><a href="link">Berita</td>
+							<td><a href="link">produk</td>
+								<td><a href="link">Album</td>
+									<td><a href="https://www.youtube.com/">youtube</td>
+							<td><a href="https://www.instagram.com/s23_seliger///">instagram</td>
+					</tr>
+					
+				</table>
+			</th>
+		</tr>
+
+		</thead>
+		<table border="1" width="100%">
+					<tr>
+						<tb align="left"><img src="sk.jpg"></tb>
+			</tr>
+		</table>
+
+</body>
+</html>
